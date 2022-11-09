64eeb22bbb9fe05789e5fe0ebf6d7b23b00fad30
Reno Greenleaf <greenleaf@tut.by>
2022-11-09 21:39:42 +0300

    Another test.

--- lazysignup/tests/test_middleware.py
+++ lazysignup/tests/test_middleware.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from lazysignup.models import LazyUser
 
 
 class MiddlewareTest(TestCase):
@@ -12,3 +13,12 @@ class MiddlewareTest(TestCase):
 			response = self.client.get('/lazy-user-available')
 
 		self.assertEqual(response.status_code, 200)
+
+	def test_decorated(self):
+		"""The decorator and the middleware together shouldn't create a lazy user twice."""
+		modification = {'append': 'lazysignup.middleware.AllowLazyUser'}
+
+		with self.modify_settings(MIDDLEWARE=modification):
+			self.client.get('/lazy/')
+
+		self.assertEqual(LazyUser.objects.count(), 1)
